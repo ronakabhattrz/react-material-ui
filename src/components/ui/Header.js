ca1099295c036eab0f3d9d9e1b90980a620acb18
Ronak Bhatt <ronakabhattrz@gmail.com>
2021-05-18 14:22:18 +0530

    Header set

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/styles';
 
+import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -17,16 +19,28 @@ function ElevationScroll(props) {
     });
   }
 
+  const useStyles = makeStyles(theme => ({
+    toolbarMargin: {
+      ...theme.mixins.toolbar,
+      marginBottom: "3em"
+    },
+    logo: {
+      height: "7em" 
+    }
+  }));
+
 export default function Header(props){
+    const classes = useStyles();
     return (
-        <ElevationScroll>
+      <React.Fragment>
+        <ElevationScroll> 
             <AppBar position="fixed">
-                <Toolbar>
-                  <Typography variant="h3">
-                    Ronak Development!!!
-                  </Typography>
+                <Toolbar disableGutters>
+                  <img alt="Company Logo" className={classes.logo} src={logo}/>
                 </Toolbar>
             </AppBar>
         </ElevationScroll>
+        <div className={classes.toolbarMargin} />
+      </React.Fragment>
     )
 }
