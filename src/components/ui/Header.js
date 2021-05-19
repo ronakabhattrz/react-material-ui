313c321babd3de2e62aad77757efd4b801789d9e
Ronak Bhatt <ronakabhattrz@gmail.com>
2021-05-19 10:57:13 +0530

    Header Build

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -4,6 +4,9 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import {makeStyles} from '@material-ui/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
 
 import logo from '../../assets/logo.svg';
 
@@ -26,6 +29,21 @@ function ElevationScroll(props) {
     },
     logo: {
       height: "7em" 
+    },
+    tabContainer: { 
+      marginLeft: 'auto'
+    },
+    tab: { 
+      ...theme.typography.tab,
+      minWidth: 10,
+      marginLeft: '25px'
+    },
+    button: {
+      ...theme.typography.estimate,
+      borderRadius: '50px',
+      marginLeft: '50px',
+      marginRight: '50px',
+      height: "45px",
     }
   }));
 
@@ -37,6 +55,17 @@ export default function Header(props){
             <AppBar position="fixed">
                 <Toolbar disableGutters>
                   <img alt="Company Logo" className={classes.logo} src={logo}/>
+                  <Tabs className={classes.tabContainer}>
+                    <Tab className={classes.tab} label="Home" />
+                    <Tab className={classes.tab} label="Services" />
+                    <Tab className={classes.tab} label="The Revolution" />
+                    <Tab className={classes.tab} label="About Us" />
+                    <Tab className={classes.tab} label="Contact Us" />
+                  </Tabs>
+                  <Button variant="contained" color="secondary"
+                    className={classes.button}>
+                    Free Estimate
+                  </Button>
                 </Toolbar>
             </AppBar>
         </ElevationScroll>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -18,8 +18,17 @@ export default createMuiTheme({
         }
     },
     typography:{ 
-        h3: { 
-            fontWeight: 300
+        tab: { 
+            fontFamily: "Raleway",
+            textTransform: 'none',
+            fontWeight: 700,
+            fontSize: '1rem',
+        },
+        estimate:{ 
+            fontFamily: 'Pacifico',
+            fontSize: '1rem',
+            textTransform: 'none',
+            color: 'white'
         }
     }
 });
